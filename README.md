014841782b0a1c9a171d947bffd29c84ed693000
Andy Evans <andrewevans0@outlook.com>
2018-05-30 14:13:23 +0100

    changed routing strategy

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
+## Useful info
+
+HashLocationStrategy https://codecraft.tv/courses/angular/routing/routing-strategies/#_hashlocationstrategy
+
+Gotta love this link!
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  async getData() {
+    try {
+      const response = await fetch('https://us-central1-userddata.cloudfunctions.net/helloWorld/weatherdata');
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log('good user!!!');
+        console.log(jsonResponse);
+        return jsonResponse;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
 }
